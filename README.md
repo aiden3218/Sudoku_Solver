8ff87bbedbf42e6b3e6df06209c3ba29ca74f2f7
Aiden Thero <aident.3218@gmail.com>
2024-09-15 22:19:32 -0400

    comments and readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Sudoku_Solver
-hello
-hello
+This is a basic Sudoku solver with 2 main functions line_solver() and Squares()
+
+line_solver finds all the possible numbers for each empty square with respect to the rows and columns
+Squares finds all the possible numbers for each empty square with respect to the 3x3 squares
+
+Potential() simply runs the two other functions together and fills any empty spaces if there is only one possible number
+
+main() runs potential in a loop until the grid is finished, for the case in the code it only takes 4 times
+
+This solver can not solve all Sudokus as it can not solve multiple potential sequences, it must find spots with only one
+possible value for it to replace the value
--- python_sudoku_solver.py
+++ python_sudoku_solver.py
@@ -1,7 +1,10 @@
+import copy
 grid = [[None,None,3,5,7,None,2,None,None],[2,9,None,1,None,3,None,None,6],[None,None,None,None,None,4,1,9,None],[None,8,2,None,3,None,None,1,7],[None,None,None,7,None,8,None,None,None,],[9,1,None,None,4,None,3,6,None],[None,2,1,4,None,None,None,None,None],[7,None,None,8,None,1,None,2,5],[None,None,5,None,9,2,6,None,None]]
-#grid = [[8,None,None,None,9,None,None,7,None],[None,None,None,None,None,None,2,None,None],[None,3,1,None,6,7,None,None,5],[None,2,None,9,None,None,1,None,None],[None,6,None,5,None,2,None,4,None],[None,None,3,None,None,1,None,5,None],[3,None,None,1,5,None,8,6,None],[None,None,6,None,None,None,None,None,None],[None,8,None,None,2,None,None,None,9]]
+
+"""
+searches for any possible values by line horizontally and vertically
+"""
 def line_solver(line_segment):
-    
     if line_segment < 9:
         possibles = set()
         for i in range(1,10):
@@ -15,7 +18,9 @@ def line_solver(line_segment):
                     possibles.remove(j)
     return possibles
 
-
+"""
+searches for any possible values by the 3x3 squares in the grid
+"""
 def squares(x,y):
     possibles = set([1,2,3,4,5,6,7,8,9])
     for i in range(0,3):
@@ -25,7 +30,9 @@ def squares(x,y):
     return possibles
 
 
-
+"""
+searches for any possible values in each square adn solves the square if there is only one potential value
+"""
 def potential():
     potential = []
     for i in range(9):
@@ -43,8 +50,36 @@ def potential():
                     potential[i][j-9] = None
             else:
                 potential[i][j-9] = None
-    
+
+
+def equals(duplicate):
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] != duplicate[i][j]:
+                return False
+    return True
+
+def print_grid():
+    print(grid[0])
+    print(grid[1])
+    print(grid[2])
+    print(grid[3])
+    print(grid[4])
+    print(grid[5])
+    print(grid[6])
+    print(grid[7])
+    print(grid[8])
+    print(grid[8])
+    print("\n")
+
 
 def main():
-    potential()
+    duplicate = []
+    for i in range(9):
+        duplicate.append([None,None,None,None,None,None,None,None,None])
+    while not equals(duplicate):
+        duplicate = copy.deepcopy(grid)
+        potential()
+        print_grid()
+
 main()
